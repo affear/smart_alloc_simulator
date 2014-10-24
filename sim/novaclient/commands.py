2caba7d7f4ccdd680af58dfb9b9618709f71e79b
Lorenzo Affetti <affox90@gmail.com>
2014-10-24 17:06:40 +0200

    sample commands

--- sim/novaclient/commands.py
+++ sim/novaclient/commands.py
@@ -1,1 +1,13 @@
 from sim.nova.compute import rpcapi
+
+class _Command(object):
+	"""docstring for Command"""
+
+	def execute(self):
+		print "executing " + self.name
+
+	class Meta:
+		abstract = True
+
+class CreateCommand(_Command):
+	name = 'boot'
