350a3e9e17db11a58950665cdf780bcff9638f4e
Lorenzo Affetti <affox90@gmail.com>
2014-10-24 17:06:14 +0200

    auto extracting command classes from commands.py

--- sim/novaclient/shell.py
+++ sim/novaclient/shell.py
@@ -1,1 +1,12 @@
 from sim.novaclient import commands
+import sys
+import inspect
+
+def _filter_command_fn(obj):
+	return inspect.isclass(obj) and not obj.__name__.startswith('_') and obj.__name__.endswith('Command')
+
+CMDS = {}
+for name, cs in inspect.getmembers(commands, _filter_command_fn):
+	CMDS[name.lower()[:(len(name)-len('Command'))]] = cs()
+
+
