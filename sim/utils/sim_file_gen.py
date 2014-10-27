42b1a08cacfa812b569fbd4b25bfd23e9286ae06
Lorenzo Affetti <affox90@gmail.com>
2014-10-27 16:04:58 +0100

    v1 working

--- sim/utils/sim_file_gen.py
+++ sim/utils/sim_file_gen.py
@@ -5,21 +5,32 @@ from sim.novaclient import commands
 import random
 
 NUM_T = 10
-MAX_OPS_PER_T = 5
+NUM_PM = 5
 COMMANDS = commands.CMDS
 
 cmds_history = {
 	'history': [],
-	'opt_cfg': 'optional'
+	'cfg': {
+		'pms': NUM_PM
+		#other configuration parameters
+	}
 }
+
+status = []
 
 if __name__ == '__main__':
 	for t in xrange(NUM_T):
 		t_dict = {}
-		num_ops = random.randint(0, MAX_OPS_PER_T)
-		cmd_list = [random.choice(COMMANDS.values()).name for x in xrange(num_ops)]
+		if len(status) > 0:
+			cmd = random.choice(COMMANDS.values())().execute_virtual(status)
+		else: #there are no virtual machines... let's spawn one!
+			cmd = [COMMANDS['create']().execute_virtual(status), ]
 		k = 't%d' % t
-		t_dict[k] = cmd_list
+		t_dict[k] = cmd
 		cmds_history['history'].append(t_dict)
-		
-	print cmds_history
+
+	import json
+	with open('sim_history.json', 'w') as out:
+		json.dump(cmds_history, out)
+
+	print 'File sim_history.json succesfully generated!'
