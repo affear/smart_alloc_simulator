631c35660037f835c271c2037071f4b2f5f1ed34
Lorenzo Affetti <affox90@gmail.com>
2014-10-27 12:17:44 +0100

    sim script moved

---
+++ sim/utils/run_sim.py
@@ -0,0 +1,12 @@
+# The real simulation
+#import sim.concrete
+from sim.nova import compute, scheduler
+from sim.smart import consolidator
+
+def do_sim():
+	pass
+
+if __name__ == '__main__':
+	print "Simulation started"
+	do_sim()
+	print "Simulation ended"
