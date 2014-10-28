c56e31c2aa520a82562b7caf7ab66a5f8e4b3fde
Lorenzo Affetti <affox90@gmail.com>
2014-10-28 09:12:16 +0100

    real constructors added, fake execution added

--- sim/novaclient/commands.py
+++ sim/novaclient/commands.py
@@ -16,8 +16,34 @@ class _Command(object):
 class CreateCommand(_Command):
 	name = 'boot'
 
+	def __init__(self, flavor, id):
+		super(CreateCommand, self).__init__()
+		self.flavor = flavor
+		self.id = id
+
+	def execute(self):
+		super(CreateCommand, self).execute()
+		print ' '.join(['-->', self.flavor['name'], str(self.id)])
+
 class TerminateCommand(_Command):
 	name = 'down'
 
+	def __init__(self, id):
+		super(TerminateCommand, self).__init__()
+		self.id = id
+
+	def execute(self):
+		super(TerminateCommand, self).execute()
+		print ' '.join(['-->', str(self.id)])
+
 class ResizeCommand(_Command):
 	name = 'resize'
+
+	def __init__(self, flavor, id):
+		super(ResizeCommand, self).__init__()
+		self.flavor = flavor
+		self.id = id
+
+	def execute(self):
+		super(ResizeCommand, self).execute()
+		print ' '.join(['-->', self.flavor['name'], str(self.id)])
