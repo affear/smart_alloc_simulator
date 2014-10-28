3c133e8c9b674f8a6f4982b57f0b05c2e7085ca6
Lorenzo Affetti <affox90@gmail.com>
2014-10-28 09:13:21 +0100

    first working draft

--- sim/utils/run_sim.py
+++ sim/utils/run_sim.py
@@ -1,12 +1,22 @@
 # The real simulation
-#import sim.concrete
-from sim.nova import compute, scheduler
-from sim.smart import consolidator
-
-def do_sim():
-	pass
-
 if __name__ == '__main__':
+	import json
+	from sim.utils import HISTORY_FILE
+	from sim.utils.sim_file_gen import CMDS 
+	cmds_list = []
+	with open(HISTORY_FILE, 'r') as f:
+		data = json.load(f)
+		#TODO use cfg to create PMs and so on so forth
+		def mapping_fn(cmd_string):
+			keyword = cmd_string.split(' ')[0]
+			return CMDS[keyword]().get_concrete_command(cmd_string)
+
+		cmds_list = map(mapping_fn, data['history'])
+
+	def do_sim():
+		for cmd in cmds_list:
+			cmd.execute()
+
 	print "Simulation started"
 	do_sim()
 	print "Simulation ended"
