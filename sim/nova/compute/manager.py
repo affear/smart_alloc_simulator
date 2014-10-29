5307f3a2a418f16c44e2eaf334cd931ce2066ae4
Lorenzo Affetti <affox90@gmail.com>
2014-10-29 10:38:03 +0100

    fix in logging

--- sim/nova/compute/manager.py
+++ sim/nova/compute/manager.py
@@ -1,8 +1,9 @@
 from oslo import messaging
 from oslo.config import cfg
-from sim.nova.compute import logger
+import logging
 
 class ComputeTaskManager(object):
+	logger = logging.getLogger('compute')
 
 	def _log_info(self, task_name, *args):
 		args = list(args)
@@ -10,7 +11,7 @@ class ComputeTaskManager(object):
 		args.insert(0, task_name)
 		args.insert(0, 'executing')
 		args = map(lambda a: str(a), args)
-		logger.info(' '.join(args))
+		self.logger.info(' '.join(args))
 
 	def build_instance(self, ctx, id, flavor):
 		self._log_info('boot', id, flavor['name'])
@@ -22,10 +23,13 @@ class ComputeTaskManager(object):
 		self._log_info('resize', id, flavor['name'])
 
 if __name__ == '__main__':
+	from sim.utils import log_utils
+	log_utils.setup_logger('compute', log_utils.COMPUTE_LOG_FILE)
+
 	transport = messaging.get_transport(cfg.CONF)
 	target = messaging.Target(topic='compute', server='compute1')
 	endpoints = [ComputeTaskManager(), ]
 	server = messaging.get_rpc_server(transport, target, endpoints,
 																			executor='blocking')
 	server.start()
-	server.wait()
+	#server.wait()
