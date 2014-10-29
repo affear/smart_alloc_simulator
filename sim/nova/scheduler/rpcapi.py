b4ec78e149e7a1dd4b0d8cbc80bc58cd115ca1ce
Giacomo Bresciani <brescia123@gmail.com>
2014-10-29 11:30:21 +0100

    fixed following the openstack way

--- sim/nova/scheduler/rpcapi.py
+++ sim/nova/scheduler/rpcapi.py
@@ -1,11 +1,24 @@
-from oslo import 
+from oslo import messaging 
 from oslo.config import cfg
 
-transport = messaging.get_transport(cfg.CONF)
-target = messaging.Target(topic='scheduler')
-client = messaging.RPCClient(transport, target)
+rpcapi_opts = [
+	cfg.StrOpt(
+		'scheduler_topic',
+		default='scheduler'
+		help='The topic scheduler listen on'
+	),
+]
+
+CONF = cfg.CONF
+CONF.register_opts(rpcapi_opts)
 
 class SchedulerAPI(object):
-	
-	def select_destinations(self, id, flavor):
-		client.cast({}, 'select_destinations', id=id, flavor=flavor)
+
+	def __init__(self):
+		super(SchedulerAPI, self).__init__()
+		transport = messaging.get_transport(cfg.CONF)
+		target = messaging.Target(topic=CONF.scheduler_topic)
+		self.client = messaging.RPCClient(transport, target)
+
+	def select_destinations(self, flavor):
+		client.cast({}, 'select_destinations', flavor=flavor)
