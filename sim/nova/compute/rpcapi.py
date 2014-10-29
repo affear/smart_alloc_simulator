1afe794a4302305b780885764ab2958d449b9370
Lorenzo Affetti <affox90@gmail.com>
2014-10-29 11:21:43 +0100

    fix in RPCClient init

--- sim/nova/compute/rpcapi.py
+++ sim/nova/compute/rpcapi.py
@@ -1,20 +1,34 @@
 from oslo import messaging
 from oslo.config import cfg
 
-transport = messaging.get_transport(cfg.CONF)
-target = messaging.Target(topic='compute')
-client = messaging.RPCClient(transport, target)
+# as they do in OpenStack
+rpcapi_opts = [
+	cfg.StrOpt(
+		'compute_topic',
+		default='compute',
+		help='The topic compute nodes listen on'
+	),
+]
+
+CONF = cfg.CONF
+CONF.register_opts(rpcapi_opts)
 
 class ComputeTaskAPI(object):
+	def __init__(self):
+		super(ComputeTaskAPI, self).__init__()
+		transport = messaging.get_transport(cfg.CONF)
+		target = messaging.Target(topic=CONF.compute_topic)
+		self.client = messaging.RPCClient(transport, target)
+
 	def build_instance(self, id, flavor):
 		#TODO implement the rest
-		client.cast({}, 'build_instance', id=id, flavor=flavor)
+		self.client.cast({}, 'build_instance', id=id, flavor=flavor)
 
 	def delete(self, id):
 		#TODO implement the rest
-		client.cast({}, 'delete', id=id)
+		self.client.cast({}, 'delete', id=id)
 
 	def resize(self, id, flavor):
 		#TODO implement the rest
-		return client.call({}, 'resize', id=id, flavor=flavor)
+		return self.client.call({}, 'resize', id=id, flavor=flavor)
 
