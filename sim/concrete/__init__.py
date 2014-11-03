2deccba20eebe70f7d3a707c3e701ad2074c639d
Lorenzo Affetti <affox90@gmail.com>
2014-11-03 10:01:03 +0100

    change in pm conf

--- sim/concrete/__init__.py
+++ sim/concrete/__init__.py
@@ -1,11 +1,41 @@
 from oslo.config import cfg
+from nova import enum, METRICS
+
+PM_TYPES = enum(
+	STD_PM={
+		METRICS.VCPU: 12,
+		METRICS.RAM: 16000,
+		METRICS.DISK: 2000,
+	},
+	HALF_PM={
+		METRICS.VCPU: 6,
+		METRICS.RAM: 8000,
+		METRICS.DISK: 1000,
+	},
+	DOUBLE_PM={
+		METRICS.VCPU: 24,
+		METRICS.RAM: 32000,
+		METRICS.DISK: 4000,
+	}
+)
 
 concrete_group = cfg.OptGroup(name='concrete')
 concrete_opts = [
-	cfg.IntOpt(
-		'no_pms',
-		default=5,
-		help='The number of physical machines'
+	cfg.ListOpt(
+		'pms',
+		default=[
+			PM_TYPES.STD_PM,
+			PM_TYPES.STD_PM,
+			PM_TYPES.STD_PM,
+			PM_TYPES.STD_PM,
+			PM_TYPES.STD_PM,
+			PM_TYPES.HALF_PM,
+			PM_TYPES.HALF_PM,
+			PM_TYPES.HALF_PM,
+			PM_TYPES.HALF_PM,
+			PM_TYPES.DOUBLE_PM,
+		],
+		help='The phisycal machines used during the simulation'
 	),
 ]
 CONF = cfg.CONF
