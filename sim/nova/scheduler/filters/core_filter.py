a01447268781bf5e02610b24b87239a4795c5dd8
Giacomo Bresciani <brescia123@gmail.com>
2014-11-05 10:16:29 +0100

    First three filters

--- sim/nova/scheduler/filters/core_filter.py
+++ sim/nova/scheduler/filters/core_filter.py
@@ -2,10 +2,10 @@ from sim.nova import METRICS
 from sim.concrete import db
 from sim.scheduler.filters import BaseFilter
 
-class RamFilter(BaseFilter):
+class CoreFilter(BaseFilter):
 
 	def host_passes(self, host, flavor):
-		"""Returns True if the host is Ram-capable to handle the flavor"""
+		"""Returns True if the host has enough vcpus to handle the flavor"""
 		assert type(host) is db.Host
 
-		return host.memory_mb - host.memory_mb_used > flavor[METRICS.RAM]
+		return host.vcpus - host.vcpus_used > flavor[METRICS.VCPU]
---
+++ sim/nova/scheduler/filters/disk_filter.py
@@ -0,0 +1,12 @@
+from sim.nova import METRICS
+from sim.concrete import db
+from sim.scheduler.filters import BaseFilter
+
+class DiskFilter(BaseFilter):
+
+	def host_passes(self, host, flavor):
+		"""Returns True if the host has enough free disk space to handle the flavor"""
+		assert type(host) is db.Host
+
+		return host.local_gb - host.local_gb_used > flavor[METRICS.DISK]
+
