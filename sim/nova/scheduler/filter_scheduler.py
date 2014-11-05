0043177f0c18578b0c21365cc60c750c84ba3d6b
Giacomo Bresciani <brescia123@gmail.com>
2014-11-05 13:34:46 +0100

    first scheduler draft

--- sim/nova/scheduler/filter_scheduler.py
+++ sim/nova/scheduler/filter_scheduler.py
@@ -1,29 +1,63 @@
 from sim.concrete import db
-import sim.nova.scheduler.filters
+from sim.nova.scheduler import filters
 from oslo.config import cfg
 
+
 CONF = cfg.CONF
+CONF.import_opt('default_filters', 'sim.nova.scheduler.filters')
+
 
 class FilterScheduler(object):
 	"""FilterScheduler simulator"""
 
+	def _select_filters(self, filters):
+		if not filters:
+			selected_filters = CONF.default_filters
+		else:
+			selected_filters = filters
+		return selected_filters
+
+	def _schedule(self, all_hosts, selected_filters, flavor):
+
+		def _class_import(self, class_path):
+			"""Returns the class given its path as a string"""
+			mod_path = '.'.join(class_path.split('.')[0:-1])
+			class_name = class_path.split('.')[-1]
+			mod = __import__(mod_path)
+			components = mod_path.split('.')
+			for comp in components[1:]:
+				mod = getattr(mod, comp)
+			clazz = getattr(mod, class_name)
+			return clazz
+
+		def _filter(self, host, filters, flavor):			
+			for f in filters:
+				if f.host_passes(host, flavor):
+					passes = host
+				else:
+					passes = None
+
+		selected_hosts = []
+		filters = []
+
+		for sf in selected_filters:
+			filterClass = _class_import(f)
+			filters = filterClass()
+
+		for h in all_hosts:
+			if _filter(h, filters, flavor):
+				selected_hosts.append(h)
+
+		return selected_hosts
+
+
 	#We don't consider the possibility to pass a bundle of instances to be spawned
-	def select_destinations(self, flavor):
+	def select_destinations(self, flavor, filters=None):
+		""""Returns a list of destinations ordered from the best suitable to the worst"""
 
-		dest = []
-		all_hosts = db.get_all()
+		all_hosts = db.Host().select()
+		selected_filters = self._select_filters(filters)
 
-
+		selected_destinations = self._schedule(all_hosts, selected_filters, flavor)
 
 		return dest
-
-	def _schedule():
-
-
-	def _get_all_host_states(self):
-		""""Returns the list containing the hosts"""
-
-		hosts = []
-
-		return hosts
-
