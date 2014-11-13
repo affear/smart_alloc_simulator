82ce39b6b12df3825ba5994cd94eed095a6f5a3e
Giacomo Bresciani <brescia123@gmail.com>
2014-11-13 11:27:14 +0100

    added working chart

--- view/public/script.js
+++ view/public/script.js
@@ -3,17 +3,23 @@
 
     document.addEventListener('polymer-ready', function() {
 
-		var DEFAULT_ROUTE="graph";
+        var DEFAULT_ROUTE = "chart";
         var template = document.querySelector('#t');
-        var menuItem = document.querySelector('paper-item');
+        //var menuItem = document.querySelector('#menu-item');
+        var pages = document.querySelector('#pages');
+        var cache = {};
+        var ajax;
+
         template.pages = [{
-            title: "Graph",
-            hash: "graph",
-            icon: "trending-up"
+            title: "Consumption Chart",
+            hash: "chart",
+            icon: "trending-up",
+            url: 'pages/chart_page.html'
         }, {
             title: "Compute Log",
             hash: "compute_log",
-            icon: "toc"
+            icon: "toc",
+            url: 'logs/compute.log'
         }, {
             title: "Scheduler Log",
             hash: "scheduler_log",
@@ -26,18 +32,31 @@
             title: "Database Log",
             hash: "database_log",
             icon: "toc"
-        }
-        ];
+        }];
         template.pageTitle = template.pages[0];
         template.addEventListener('template-bound', function(e) {
             this.route = this.route || DEFAULT_ROUTE;
+            ajax = document.querySelector('#ajax');
         });
 
-        template.menuItemSelected = function (e, detail, sender) {
-			template.pageTitle = detail.item.label_;
-        	if(detail.isSelected){
-        		document.querySelector('core-scaffold').closeDrawer();
-        	}
+        template.menuItemSelected = function(e, detail, sender) {
+            if (detail.isSelected) {
+                this.async(function() {
+                    if (!cache[ajax.url]) {
+                        ajax.go();
+                    }
+                    document.querySelector('core-scaffold').closeDrawer();
+                });
+
+            }
         }
+        template.onResponse = function(e, detail, sender) {
+            var html = detail.response;
+            cache[ajax.url] = html; // Primitive caching by URL.
+            var pages = document.querySelector('#pages');
+            this.injectBoundHTML(html,
+                pages.selectedItem.firstElementChild);
+        };
+
     });
 })(wrap(document))
