5b01094025a8689cdd7b88e1464f24244e9e7295
Lorenzo Affetti <affox90@gmail.com>
2014-11-17 12:04:46 +0100

    load file method created. init_conf executes load_file plus rpc init

--- sim/config.py
+++ sim/config.py
@@ -4,6 +4,9 @@ from sim.nova import rpc
 DEFAULT_CONFIG_FILE = 'sim.conf'
 CONF = cfg.CONF
 
-def init_conf(): # in OpenStack, it is parse_args()
+def load_conf_file():
 	CONF(default_config_files=[DEFAULT_CONFIG_FILE, ])
+
+def init_conf(): # in OpenStack, it is parse_args()
+	load_conf_file()
 	rpc.init(CONF)
